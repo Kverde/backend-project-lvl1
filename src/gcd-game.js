0be993bae55c6b9969c188fed712867401412143
KonstantinSh <Kverde@yandex.ru>
2021-07-18 16:54:38 +0500

    Добавлена игра НОД

---
+++ src/gcd-game.js
@@ -0,0 +1,25 @@
+import { print } from './cli.js';
+import { getRandomInt, gcd } from './utils.js';
+import { startGame } from './index.js';
+
+const describeEvenGame = () => {
+  print('Find the greatest common divisor of given numbers.');
+};
+
+const MAX_NUMBER = 100;
+
+const genQuestion = () => {
+  const a = getRandomInt(MAX_NUMBER + 1);
+  const b = getRandomInt(MAX_NUMBER + 1);
+  const correctAnswer = gcd(a, b);
+
+  const question = `${a} ${b}`;
+
+  return { question, correctAnswer };
+};
+
+const gcdGame = () => {
+  startGame(describeEvenGame, genQuestion);
+};
+
+export default gcdGame;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 
 export const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+export const gcd = (a, b) => {
+  if (!b) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+};
